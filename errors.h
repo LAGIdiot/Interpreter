c1e8f358d25edb3bac3040f19c936447984f09e6
mmalrik <mmalrik@gmail.com>
2015-10-04 11:32:46 +0200

    error.h : hlavičkový subor k errors.c, obsahuje všetko čo erik vytknul a makro #IFNDEF, dve funkcie vypisujuce chybu a ukončujú program. error.c : uprava funkcie mistake s osetrenym defaultu a chyby 99, a ich nahradenym a osetrenym pripadnej blbosti ( nekdo zada misto 99 , 88 tak exit vypluje 99 ) , vytvorenie funkcie mstk_debug ktora ma 3 parametre a bude sa ppouzivat vramci testovania , zahrnute aj komentare , vyjadrite sa , na pripadne problemy , opravim

---
+++ errors.h
@@ -0,0 +1,24 @@
+#ifndef ERRORS_H
+#define ERRORS_H
+
+#include "errors.c"
+
+
+enum mistakes {
+    ERR_LEX = 1,
+    ERR_SYNTAX = 2,
+    ERR_SEM_UND = 3,
+    ERR_SEM_COMP = 4,
+    ERR_SEM_TYP = 5,
+    ERR_SEM_OTH = 6,
+    ERR_LOAD = 7,
+    ERR_INIT = 8,
+    ERR_ZERO = 9,
+    ERR_RUN_OTH = 10,
+    ERR_INTERN = 99
+};
+
+void mistake (int param1);
+void mstk_debug (int param1, int param2, int param3);
+
+#endif
