1dd17d667d4bb5a69853764a577539859708b446
mmalrik <mmalrik@gmail.com>
2015-10-14 09:24:53 +0200

    ail.c ial.h vlozil som 2 funkcie co ktore sa tykaju KMP algoritmu 1. tvori tabulku pre prvky vzoru 2. tvori vyhladavaciu funkciu kmp

--- ial.h
+++ ial.h
@@ -1,6 +1,22 @@
 #ifndef IAL_Header
 #define IAL_Header
 
+///////////
+// funkcia *compute_prefix_function ( urobi si tabulku pismen  a nastavi hodnoty, vytvori pamet pre tabulku )
+//char *pattern - pattern je hladani vzor , pole
+//int psize - dlzka vzoru
+///////////
+int *compute_prefix_function(char *pattern, int psize);
+
+///////////
+// funkcia kmp ( algoritmus na vyhladavanie v retazcoch podretazec, vzor, vrati poziciu prveho vyskytu prveho pismenka vzoru )
+//char *target - target je cielovy retazec, pole
+//int tsize - dlzka cieloveho retazca, pola
+//char *pattern - pattern je hladani vzor , pole
+//int psize - dlzka vzoru
+///////////
+int kmp(char *target, int tsize, char *pattern, int psize);
+
 
 
 #endif //IAL_Header
